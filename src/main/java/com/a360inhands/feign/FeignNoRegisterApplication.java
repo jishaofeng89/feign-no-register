dbe019352408c48230a1c6bb9e5937ffeedb576d
jishaofeng89 <1195957057@qq.com>
2020-12-02 16:44:18 +0800

    first commit

---
+++ src/main/java/com/a360inhands/feign/FeignNoRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.a360inhands.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignNoRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignNoRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a360inhands/feign/FeignNoRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a360inhands.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignNoRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
